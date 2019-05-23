5c28cc74db7262c4db03f7d071301c15c0c9b358
Ryan Bravo <rrbravo2@gmail.com>
2019-05-23 20:57:21 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -14,7 +14,7 @@ def first_challenge
   }
 
   contacts.each do |person, data|
-    data.each do |attribute, data|
+    data.each do |attribute, value|
       if attribute == :favorite_icecream_flavors
         value.delete_if {|flavor| flavor == "strawberry"}
 
